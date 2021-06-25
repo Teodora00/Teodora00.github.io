f808ef9bebeaac2c2709655cf8526b97e2da3908
Teodora00 <64340126+Teodora00@users.noreply.github.com>
2021-06-25 20:48:37 +0200

    Add files via upload

---
+++ primes_bellow.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    if n < 1:
+        return False
+    elif n >= 1:
+         for x in range (2, n):
+            if n % x == 0:
+                return False
+    else:
+        return True    
+
+def primes_bellow (n):
+    primes = []
+    for x in range(2, n):
+        if is_prime(x) == True:
+            primes.append(x)
+            return print(primes)
+
+primes_bellow(18)
